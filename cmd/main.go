e531e088757e50e58fb53bd89c60cc51385cf79e
krishna <krishanpal.singh@finbox.in>
2024-09-26 17:37:18 +0530

    fix:removed secret key

--- cmd/main.go
+++ cmd/main.go
@@ -50,7 +50,7 @@ func main() {
 
 	defer dbService.Close()
 
-	openaiClient := openai.NewClient("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")
+	openaiClient := openai.NewClient("sk")
 
 	externalChatGptClient := externalOpenAiClient.NewOpenAiClient(openaiClient)
 	httpClient := external.NewHTTPClient(5 * time.Second)
